6fa31e003e27130c67f56870d08a4d426c68a39e
Sakib Rahman <seu.sakib@gmail.com>
2026-02-02 23:46:12 +0600

    implement createBooking controller

--- src/modules/booking/booking.controller.ts
+++ src/modules/booking/booking.controller.ts
@@ -1,1 +1,28 @@
-// TODO: implement booking controller
+import type { NextFunction, Response } from "express";
+import { sendResponse } from "../../lib/responseHandler";
+import type { AuthenticatedRequest } from "../../middlewares/auth";
+import { bookingService } from "./booking.service";
+
+const createBooking = async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const userId = req.user.id;
+        const bookingData = {
+            ...req.body,
+            student_id: userId,
+        };
+        const data = await bookingService.createBooking(bookingData);
+        return sendResponse(res, {
+            statusCode: 201,
+            success: true,
+            message: "Booking created successfully",
+            data,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+export const bookingController = { createBooking };
