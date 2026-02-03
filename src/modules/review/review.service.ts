dee3e46288d00242bdbc632cc203f35c917d3048
Sakib Rahman <seu.sakib@gmail.com>
2026-02-03 11:47:55 +0600

    implement when create review parallelly make booking status marked completed

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,8 +1,14 @@
-import type { Review } from "../../../generated/prisma/client";
+import { BookingStatus, type Review } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
 const createReview = async (payload: Review) => {
-    return await prisma.review.create({ data: payload });
+    return await prisma.$transaction(async (tx) => {
+        await tx.booking.update({
+            where: { booking_id: payload.booking_id },
+            data: { status: BookingStatus.COMPLETED },
+        });
+        await tx.review.create({ data: payload });
+    });
 };
 
 const getReviewDetails = async (reviewId: string) => {
