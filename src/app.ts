ab86b21f478f272c2a320c14291d09fa0d8ca074
Sakib Rahman <seu.sakib@gmail.com>
2026-02-03 21:50:54 +0600

    added cors origin

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 import express, { type Application, type Response } from "express";
 import { auth } from "./lib/auth";
 import errorHandler from "./middlewares/globalErrorHandler";
 import notFound from "./middlewares/notFound";
 import v1Routes from "./routes/v1";
-
 const app: Application = express();
 
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
