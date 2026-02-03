c479aed42c999e40d660731327ec9030aa46753c
Sakib Rahman <seu.sakib@gmail.com>
2026-02-03 11:04:21 +0600

    implement createReview Api

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,1 +1,23 @@
-// TODO: implement review controller
+import type { NextFunction, Request, Response } from "express";
+import { sendResponse } from "../../lib/responseHandler";
+import { reviewService } from "./review.service";
+
+const createReview = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const data = await reviewService.createReview(req.body);
+        return sendResponse(res, {
+            statusCode: 200,
+            success: true,
+            message: "Create Review Successfully",
+            data,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const reviewController = { createReview };
--- src/modules/review/review.router.ts
+++ src/modules/review/review.router.ts
@@ -1,5 +1,10 @@
 import express, { type Router } from "express";
+import { UserRoles } from "../../../generated/prisma/enums";
+import auth from "../../middlewares/auth";
+import { reviewController } from "./review.controller";
 
 const router: Router = express.Router();
+
+router.post("/", auth(UserRoles.STUDENT), reviewController.createReview);
 
 export default router;
